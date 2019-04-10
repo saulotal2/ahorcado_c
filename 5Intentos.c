f7433aba5eb281eb12e288c32a2c75dbe2e967d3
saulotal2 <49192928+saulotal2@users.noreply.github.com>
2019-04-10 11:51:36 +0200

    Add files via upload

---
+++ 5Intentos.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+void cincoIntentos(int intentos, char palabra[], char prueba[], int errores, int encontradas){
+
+  printf("Tienes %i intentos\n", intentos);
+    for (int i = 0; i < strlen(palabra); i++) {
+      printf("Escribe una palabra, cada letra fallida es un error\n");
+      scanf("%s", prueba);
+
+    for (int j = i; j < strlen(palabra); j++) {
+      if(prueba[j]==palabra[j]){
+        encontradas+=1;
+        printf("Has encontrado %i letras\n", encontradas );
+        if (encontradas==strlen(palabra)) {
+          printf("Enhorabuena has ganado\n");
+          return 0;
+        }
+    }else{
+      errores+=1;
+      if(errores==intentos){
+        printf("Fin de la partida\n");
+        return 0;
+}
+    }
+
+}
+
+}
+
+}
